bca0ebb4c81d455df4d77ee32fff72b8b9c46505
saisatwik21 <saisatwik175@gmail.com>
2025-09-20 17:48:56 +0530

    INC1234 || EC2 CREATEs

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket          = "terraform-pratice-1 "          # Replace with your S3 bucket
+    bucket          = "terraform-pratice-1"          # Replace with your S3 bucket
     key             = "first"
     region          = "us-east-1"                          # Replace with your AWS region
     encrypt         = true                                 # Server-side encryption
